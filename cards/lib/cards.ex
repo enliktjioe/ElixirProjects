127208d40c9b6f85b3795163d11c42e5ced6fcd3
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-26 15:40:52 +0700

    A List Comprehension (hard code)

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,15 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # Goal to implement all 4 elements for each values
+    # for example: Ace Spades, Ace Clubs, Ace Hearts, and Ace Diamonds
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   def shuffle(deck) do
