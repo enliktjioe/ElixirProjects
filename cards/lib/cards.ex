bc64a3ef60128a72c5e6c25fde0682339f74eae1
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-26 13:44:10 +0700

    Elixir simple hello world

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,6 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "hi there!"
   end
+  
 end
