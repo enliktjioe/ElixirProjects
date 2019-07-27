d862fc5f2aafedc1ac75b22e231072c78af96cbc
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-27 11:57:37 +0700

    Saving and Load Deck

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -21,6 +21,25 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
+
+  # bad practice in Elixir
+  # def load() do
+  #   {status, binary} = File.read(filename)
+  #
+  #   if(status == :error) {
+  #     return "Something went wrong"
+  #   }
+  # end
 end
 
 # { *hand*, *deck* }
