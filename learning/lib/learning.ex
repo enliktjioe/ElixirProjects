db8a3e3eb02929d82d9fb4545e088809e89d1d48
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-29 22:41:58 +0700

    Lecture 2 Agile Software Development UT

--- learning/lib/learning.ex
+++ learning/lib/learning.ex
@@ -1,4 +1,13 @@
 defmodule Learning do
-  def factorial(0), do: 1
-  def factorial(n) when n > 0, do: n * factorial(n-1)
+  # def factorial(0), do: 1
+  # def factorial(n) when n > 0, do: n * factorial(n-1)
+
+  def print([h|t]) do
+    IO.puts h
+    print t
+  end
+
+  def print([]) do
+    IO.puts "<done>"
+  end
 end
