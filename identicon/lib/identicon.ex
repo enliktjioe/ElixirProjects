6e38adf850a7177f4646449a2c997caa75d7746c
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-29 20:24:29 +0700

    Building the Grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,14 +3,30 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     # First 3 values and don't care about the rest of values (tail)
 
-    [r, g, b]
+    %Identicon.Image{image | color: {r, g, b}}
   end
+
+  # JavaScript example code below
+  # pick_color: function(image) {
+  #   image.color = {
+  #     r: image.hex[0],
+  #     g: image.hex[1],
+  #     b: image.hex[2]
+  #   };
+  #
+  #   return image
+  # }
 
   def hash_input(input) do
     hex =
