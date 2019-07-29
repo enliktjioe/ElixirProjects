dec6d482f871b4c1c7f53b1b6b815e751ab323b5
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-29 20:00:45 +0700

    Pattern Matching Structs

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,15 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    # First 3 values and don't care about the rest of values (tail)
+
+    [r, g, b]
   end
 
   def hash_input(input) do
