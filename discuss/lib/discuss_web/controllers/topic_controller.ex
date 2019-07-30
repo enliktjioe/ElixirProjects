b2f9b7605b2ad81acfc0ef3a3d524f2d47ee619c
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-30 16:37:36 +0700

    Generating Changesets for the Form

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,12 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  alias Discuss.Topic
+
   def new(conn, params) do
-    
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
   end
 
 end
