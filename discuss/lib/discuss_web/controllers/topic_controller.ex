f37dba04eafb4c188285fd4381f7f2880338e879
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-30 16:58:41 +0700

    some compilation erro

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,10 +1,17 @@
-defmodule DiscussWeb.TopicController do
+defmodule Discuss.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"topic" => topic}) do
+
+  end
+
 
 end
