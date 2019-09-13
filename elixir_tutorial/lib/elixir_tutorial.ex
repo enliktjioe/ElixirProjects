46eba349960e9cb0b4ddaac7de2d0cdabd3ac381
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-13 22:39:30 +0300

    String manipulation

--- elixir_tutorial/lib/elixir_tutorial.ex
+++ elixir_tutorial/lib/elixir_tutorial.ex
@@ -7,7 +7,13 @@ defmodule M do
     my_str = "Enlik Enlik"
     IO.puts "Length : #{String.length(my_str)}"
     longer_str = my_str <> " " <> "is longer"
-    IO.puts "Equal : #{"Egg" === "egg"}"
+    IO.puts "En ? #{String.contains?(my_str, "En")}"
+
+    IO.puts "First : #{String.first(my_str)}"
+
+    IO.puts "Index 4 : #{String.at(my_str, 4)}"
+
+    IO.puts "Substring : #{String.slice(my_str, 3, 8)}"
   end
 
 
