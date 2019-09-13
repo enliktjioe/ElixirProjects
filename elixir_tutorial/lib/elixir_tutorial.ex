c6380cabf68464a0722faac2ace61e13efad1255
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-13 22:55:09 +0300

    switch case and if else inside #{}

--- elixir_tutorial/lib/elixir_tutorial.ex
+++ elixir_tutorial/lib/elixir_tutorial.ex
@@ -6,24 +6,13 @@ defmodule M do
   def do_stuff do
     age = 16
 
-    if age >= 18 do
-      IO.puts "Can Vote"
-    else
-      IO.puts "Can't Vote"
+    case 2 do
+      1 -> IO.puts "Entered 1"
+      2 -> IO.puts "Entered 2"
+      _ -> IO.puts "Default"
     end
 
-    unless age === 18 do
-      IO.puts "You're not 18"
-    else
-      IO.puts "You are 18"
-    end
-
-    cond do
-      age >= 18 -> IO.puts "Can Vote"
-      age >= 16 -> IO.puts "You can drive"
-      age >= 14 -> IO.puts "You can wait"
-      true -> IO.puts "Default"
-    end
+    IO.puts "Ternary #{if age > 18, do: "Can Vote", else: "Can't vote"}"
   end
 
 
