701e18145b8a391838253fd361f078176cfc0259
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-15 02:00:30 +0300

    Finish dna_analysis

--- ut-asd/dna_analysis/lib/dna_analysis.ex
+++ ut-asd/dna_analysis/lib/dna_analysis.ex
@@ -1,2 +1,17 @@
 defmodule DnaAnalysis do
+  def count('', _), do: 0
+
+  def count([head | tail], n) when head == n, do: 1 + count(tail, n)
+
+  def count([_ | tail], n), do: count(tail, n)
+
+  def histogram(chain) do
+    %{
+      ?A => count(chain, ?A),
+      ?T => count(chain, ?T),
+      ?C => count(chain, ?C),
+      ?G => count(chain, ?G)
+    }
+  end
+
 end
