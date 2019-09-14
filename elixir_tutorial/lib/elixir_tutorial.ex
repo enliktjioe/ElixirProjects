2b758f3340cd9a4312101cc19ae3dd0d0860a4b6
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-14 21:06:08 +0300

    Concurrency

--- elixir_tutorial/lib/elixir_tutorial.ex
+++ elixir_tutorial/lib/elixir_tutorial.ex
@@ -4,15 +4,33 @@ defmodule M do
   end
 
   def do_stuff do
-    
-    err = try do
-      5 / 0
 
-    rescue
-      ArithmeticError -> "Can't Divide by Zero"
+    spawn(fn() -> loop(50, 1) end)
+    spawn(fn() -> loop(100, 50) end)
+
+    send(self(), {:german, "Bob"})
+
+    receive do
+      {:german, name} -> IO.puts "Guten tag #{name}"
+      {:french, name} -> IO.puts "Bonjour #{name}"
+      {:english, name} -> IO.puts "Hello #{name}"
+
+    after
+      500 -> IO.puts "Time up"
     end
 
-    IO.puts err
+
+  end
+
+  def loop(0,_), do: nil
+
+  def loop(max, min) do
+    if max < min do
+      loop(0, min)
+    else
+      IO.puts "Num: #{max}"
+      loop(max - 1, min)
+    end
   end
 
   def display_list([word|words]) do
