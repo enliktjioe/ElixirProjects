4379d9f8650f1f49a2febef2bb2502888031ef24
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-14 20:55:53 +0300

    Exception Handler

--- elixir_tutorial/lib/elixir_tutorial.ex
+++ elixir_tutorial/lib/elixir_tutorial.ex
@@ -5,11 +5,14 @@ defmodule M do
 
   def do_stuff do
     
-    dbl_list = for n <- [1,2,3], do: n * 2
-    IO.inspect dbl_list
+    err = try do
+      5 / 0
 
-    even_list = for n <- [1,2,3,4], rem(n,2) == 0, do: n
-    IO.inspect even_list
+    rescue
+      ArithmeticError -> "Can't Divide by Zero"
+    end
+
+    IO.puts err
   end
 
   def display_list([word|words]) do
