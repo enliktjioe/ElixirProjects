bb404ca71e462fa737ea0fb4ffc87b781fd0a41f
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-09-20 15:15:13 +0300

    Finished test: "gutter game", "'all ones' game", "one spare"

--- ut-asd/bowling/lib/bowling.ex
+++ ut-asd/bowling/lib/bowling.ex
@@ -1,5 +1,28 @@
 defmodule Bowling do
   def score(game) do
+    List.flatten(game)
+    |> Enum.filter(fn e -> is_number(e) end)    # &is_number/1
+    # |> Enum.reduce(0, fn n, acc -> n + acc end) # &+/2
+    |> count()
+
+    # Enum.reduce(0, fn n, acc -> n + acc end)
+    # 0 is the initial value, acc = accumulator -> n + acc
+  end
+
+
+  defp count([]) do
     0
   end
+
+  defp count([h1 | [h2 | tail]]) when h1 + h2 == 10 do
+    10 + hd(tail) + count(tail)
+  end
+
+  defp count([h1 | [h2 | tail]]) do
+    h1 + h2 + count(tail)
+  end
+
+
+  # hd(tail) = head of tail
+  # defp = define private function
 end
--- ut-asd/bowling/test/bowling_test.exs
+++ ut-asd/bowling/test/bowling_test.exs
@@ -5,4 +5,20 @@ defmodule BowlingTest do
     game = List.duplicate([0, 0], 9) ++ [[0, 0, nil]]
     assert Bowling.score(game) == 0
   end
+
+  test "'all ones' game" do
+    game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
+    assert Bowling.score(game) == 20
+  end
+
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
+    # the score for the frame is 10 plus the number of pins knocked down in the first turn of the next frame.
+  end
+
+  # test "one strike" do
+  #   game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+  #   assert Bowling.score(game) == 24
+  # end
 end
