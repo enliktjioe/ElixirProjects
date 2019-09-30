39ba5c2126294c6e64945c991232e1c8e877f6c5
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-01 01:54:39 +0300

    Finished part 2

--- ut-asd/yahtzee/lib/yahtzee.ex
+++ ut-asd/yahtzee/lib/yahtzee.ex
@@ -1,4 +1,21 @@
 defmodule Yahtzee do
+  def generate(die_face, occurrences) do
+    Enum.to_list(1..6)
+    |> List.delete(die_face)
+    |> Enum.shuffle
+    |> Enum.take(5 - occurrences)
+    |> Enum.concat(List.duplicate(die_face, occurrences))
+    |> Enum.shuffle
+  end
+
+  def enumMap do
+    Enum.map(2..6, fn (dice_face) ->
+      dices = generate(dice_face, 3)
+      IO.puts dices
+      Enum.sum(dices)
+    end)
+  end
+
   def score_upper(dice) do
     # %{Ones: 1}
     %{Ones: length(Enum.filter(dice, fn e -> e == 1 end)),
@@ -9,7 +26,8 @@ defmodule Yahtzee do
       Sixes: length(Enum.filter(dice, fn e -> e == 6 end))}
   end
 
-  # def score_upper(dice) do
-  #   %{Twos: length(Enum.filter(dice, fn e -> e == 2 end))}
-  # end
+  def score_lower(dices) do
+    %{"Three of a kind": Enum.sum(dices)}
+  end
+
 end
