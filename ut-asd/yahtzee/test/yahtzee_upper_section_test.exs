b51d307af13c0c868e9b3983fff88b573185b8d8
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-01 01:24:15 +0300

    Finished Part 1

--- ut-asd/yahtzee/test/yahtzee_upper_section_test.exs
+++ ut-asd/yahtzee/test/yahtzee_upper_section_test.exs
@@ -1,6 +1,5 @@
 defmodule YahtzeeUpperSectionTest do
   use ExUnit.Case
-  doctest Yahtzee
 
   def generate(die_face, occurrences) do
     Enum.to_list(1..6)
@@ -18,5 +17,18 @@ defmodule YahtzeeUpperSectionTest do
   test "works with any combination of dice, containing 1-5 ones" do
     Enum.map(1..5, fn n -> assert %{Ones: ^n} = Yahtzee.score_upper(generate(1, n)) end)
   end
+
+  test "works with twos" do
+    Enum.map(1..5, fn n -> assert %{Twos: ^n} = Yahtzee.score_upper(generate(2,n)) end)
+  end
+
+  test "works on upper section, with all the other cases" do
+    Enum.map(1..5, fn n -> assert %{Threes: ^n} = Yahtzee.score_upper(generate(3, n)) end)
+    Enum.map(1..5, fn n -> assert %{Fours: ^n} = Yahtzee.score_upper(generate(4, n)) end)
+    Enum.map(1..5, fn n -> assert %{Fives: ^n} = Yahtzee.score_upper(generate(5, n)) end)
+    Enum.map(1..5, fn n -> assert %{Sixes: ^n} = Yahtzee.score_upper(generate(6, n)) end)
+  end
+
+
 end
 
