c1868ab3404572c002e6f6eec5410c1fe3e76387
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-01 10:43:45 +0300

    Update scoring system for Twos .. Sixes

--- ut-asd/yahtzee/lib/yahtzee.ex
+++ ut-asd/yahtzee/lib/yahtzee.ex
@@ -60,11 +60,11 @@ defmodule Yahtzee do
 
   def score_upper(dice) do
     %{Ones: length(Enum.filter(dice, fn e -> e == 1 end)),
-      Twos: length(Enum.filter(dice, fn e -> e == 2 end)),
-      Threes: length(Enum.filter(dice, fn e -> e == 3 end)),
-      Fours: length(Enum.filter(dice, fn e -> e == 4 end)),
-      Fives: length(Enum.filter(dice, fn e -> e == 5 end)),
-      Sixes: length(Enum.filter(dice, fn e -> e == 6 end))}
+      Twos: 2 * length(Enum.filter(dice, fn e -> e == 2 end)),
+      Threes: 3 * length(Enum.filter(dice, fn e -> e == 3 end)),
+      Fours: 4 * length(Enum.filter(dice, fn e -> e == 4 end)),
+      Fives: 5 * length(Enum.filter(dice, fn e -> e == 5 end)),
+      Sixes: 6 * length(Enum.filter(dice, fn e -> e == 6 end))}
   end
 
   def score_lower(dices) do
