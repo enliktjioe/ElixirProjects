711e154c1224b42405f875c66a241b1d549a5337
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-04 15:48:52 +0300

    Add propose recommendation from mix phx.gen.html command Add resoureces bla bla bla to lib/takso_web/router.ex

--- ut-asd/takso/lib/takso_web/router.ex
+++ ut-asd/takso/lib/takso_web/router.ex
@@ -17,6 +17,10 @@ defmodule TaksoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController
+
+    # post "/users", UserController, :new
+    # delete "/users", UserController, :delete
   end
 
   # Other scopes may use custom stacks.
