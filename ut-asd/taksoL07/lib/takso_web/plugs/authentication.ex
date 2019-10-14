713415b116b5c03cd1df1d495ae54dfb7534e013
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-15 01:14:14 +0300

    Part 2 - Adding a login page

--- ut-asd/taksoL07/lib/takso_web/plugs/authentication.ex
+++ ut-asd/taksoL07/lib/takso_web/plugs/authentication.ex
@@ -4,8 +4,26 @@ defmodule Takso.Authentication do
   def init(opts), do: opts[:repo]
 
   def call(conn, repo) do
-    IO.puts "HI THERE ..."
-    user = repo.get(Takso.Accounts.User, 1)
+    # IO.puts "HI THERE ..."
+    # user = repo.get(Takso.Accounts.User, 1)
+    # assign(conn, :current_user, user)
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Takso.Accounts.User, user_id)
+    login(conn, user_id, user)
+  end
+
+  def login(conn, user_id, user) do
     assign(conn, :current_user, user)
+    |> put_session(:user_id, user_id)
+  end
+
+  def check_credentials(conn, username, password, [repo: repo]) do
+    user = repo.get_by(Takso.Accounts.User, username: username)
+    if user.password == password do
+      # {:ok, assign(conn, :current_user, user) |> put_session(:user_id, user.id)}
+      {:ok, login(conn, user.id, user)}
+    else
+      {:error, :unauthorized, conn}
+    end
   end
 end
