1872e4dde7357ce912f81a2295a34d28c1218dbe
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-15 00:58:13 +0300

    Part 1 - Associating a default user to the session

---
+++ ut-asd/taksoL07/lib/takso_web/plugs/authentication.ex
@@ -0,0 +1,11 @@
+defmodule Takso.Authentication do
+  import Plug.Conn
+
+  def init(opts), do: opts[:repo]
+
+  def call(conn, repo) do
+    IO.puts "HI THERE ..."
+    user = repo.get(Takso.Accounts.User, 1)
+    assign(conn, :current_user, user)
+  end
+end
