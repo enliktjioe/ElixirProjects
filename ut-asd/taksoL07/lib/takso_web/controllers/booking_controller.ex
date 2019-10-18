dbad103ed188805eab8630acc3babda923eacac5
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-18 13:55:29 +0300

    P06 Starting Point

--- ut-asd/taksoL07/lib/takso_web/controllers/booking_controller.ex
+++ ut-asd/taksoL07/lib/takso_web/controllers/booking_controller.ex
@@ -5,6 +5,13 @@ defmodule TaksoWeb.BookingController do
   alias Takso.Sales.Taxi
   alias Takso.Sales.Booking
   alias Takso.Repo
+  alias Ecto.{Changeset, Multi}
+  alias Takso.Sales.{Taxi, Booking, Allocation}
+
+  def index(conn, _params) do
+    bookings = Repo.all(from b in Booking, where: b.user_id == ^conn.assigns.current_user.id)
+    render conn, "index.html", bookings: bookings
+  end
 
   def new(conn, _params) do
     changeset = Booking.changeset(%Booking{}, %{})
@@ -15,18 +22,31 @@ defmodule TaksoWeb.BookingController do
     user = conn.assigns.current_user
 
     booking_struct = Ecto.build_assoc(user, :bookings, Enum.map(booking_params, fn({key, value}) -> {String.to_atom(key), value} end))
-    Repo.insert(booking_struct)
+    changeset = Booking.changeset(booking_struct, %{})
+                |> Changeset.put_change(:status, "open")
+
+    booking = Repo.insert!(changeset)
 
     query = from t in Taxi, where: t.status == "available", select: t
     available_taxis = Repo.all(query)
 
     case length(available_taxis) > 0 do
-      true -> conn
+      true -> taxi = List.first(available_taxis)
+              Multi.new
+              |> Multi.insert(:allocation, Allocation.changeset(%Allocation{}, %{status: "accepted"}) |> Changeset.put_change(:booking_id, booking.id) |> Changeset.put_change(:taxi_id, taxi.id))
+              |> Multi.update(:taxi, Taxi.changeset(taxi, %{}) |> Changeset.put_change(:status, "busy"))
+              |> Multi.update(:booking, Booking.changeset(booking, %{}) |> Changeset.put_change(:status, "allocated"))
+              |> Repo.transaction
+
+              conn
               |> put_flash(:info, "Your taxi will arrive in 5 minutes")
-              |> redirect(to: booking_path(conn, :new))
-      _    -> conn
+              |> redirect(to: booking_path(conn, :index))
+      _    -> Booking.changeset(booking) |> Changeset.put_change(:status, "rejected")
+              |> Repo.update
+
+              conn
               |> put_flash(:info, "At present, there is no taxi available!")
-              |> redirect(to: booking_path(conn, :new))
+              |> redirect(to: booking_path(conn, :index))
     end
   end
 
