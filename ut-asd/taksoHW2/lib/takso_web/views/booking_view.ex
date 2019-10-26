3f961a46627fef4f017c857801ed0c446ac9fa7b
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-10-26 16:10:15 +0300

    Skeleton project for takso HW2 based on P06 finished project

---
+++ ut-asd/taksoHW2/lib/takso_web/views/booking_view.ex
@@ -0,0 +1,6 @@
+defmodule TaksoWeb.BookingView do
+  use TaksoWeb, :view
+end
+defmodule TaksoWeb.BookingView do
+  use TaksoWeb, :view
+end
---
+++ ut-asd/taksoHW2/test/takso_web/controllers/booking_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule TaksoWeb.BookingControllerTest do
+  use TaksoWeb.ConnCase
+
+  alias Takso.{Repo,Sales.Taxi}
+
+  test "Booking rejection", %{conn: conn} do
+    Repo.insert!(%Taxi{status: "busy"})
+    conn = post conn, "/bookings", %{booking: [pickup_address: "Liivi 2", dropoff_address: "Lõunakeskus"]}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/At present, there is no taxi available!/
+  end
+
+  test "Booking aceptance", %{conn: conn} do
+    Repo.insert!(%Taxi{status: "available"})
+    conn = post conn, "/bookings", %{booking: [pickup_address: "Liivi 2", dropoff_address: "Lõunakeskus"]}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Your taxi will arrive in \d+ minutes/
+  end
+end
